aa25bdd7062493c4c315a240d76ad289ae612d98
Dmitry <dmitry27zh@gmail.com>
2023-10-01 13:24:00 +0300

    Functions

---
+++ dist/01.primitives.js
@@ -0,0 +1,40 @@
+"use strict";
+// number
+let x = 10.5;
+// let y = 123n
+let z = NaN;
+// string
+let str1 = 'Hello World!';
+// implicit
+let str = 'asdfg';
+// str = 4 ---- error
+// explicit
+// any
+let str2;
+str2 = 2;
+let str3;
+str3 = '3';
+// symbol
+let symb = Symbol('1');
+// strong typing
+// symb + str3 ---- error
+// boolean
+let b = true;
+// b = 'false' ---- error
+// nothing
+let nothing1 = undefined;
+let nothing2 = null;
+// literal
+const num = 100; // implicit type
+// universal
+let any = 2;
+any = '1';
+any = false;
+any = 'string';
+any.toUpperCase();
+// unknown
+let smt = '1';
+// smt.toUpperCase() ---- error
+if (typeof smt === 'string') {
+    smt.toUpperCase();
+}
