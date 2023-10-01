f00800c83d453b3ad471b30c99630862b6217001
Dmitry <dmitry27zh@gmail.com>
2023-10-01 14:13:55 +0300

    Object type

---
+++ dist/03. object.js
@@ -0,0 +1,14 @@
+"use strict";
+const car = {
+    wheels: 4,
+    brand: 'BMW',
+    type: 'sedan',
+};
+// car.brand = {} ---- error
+const car2 = {
+    brand: 'BMW',
+    type: '',
+    wheels: 4,
+};
+car2.go = true; // ---- no error
+car2['go'] = false; // ---- styleguide
---
+++ src/03. object.ts
@@ -0,0 +1,25 @@
+interface Car {
+  wheels: number
+  brand: string
+  type: string
+  isNew?: boolean
+  name?: string
+  [key: string]: unknown
+}
+
+const car: Car = {
+  wheels: 4,
+  brand: 'BMW',
+  type: 'sedan',
+}
+
+// car.brand = {} ---- error
+
+const car2: Car = {
+  brand: 'BMW',
+  type: '',
+  wheels: 4,
+}
+
+car2.go = true // ---- no error
+car2['go'] = false // ---- styleguide
