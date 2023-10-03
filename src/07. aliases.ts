ca1df2c132d69821fdd8ffb4fa313a311ae75856
Dmitry <dmitry27zh@gmail.com>
2023-10-03 10:56:43 +0300

    Aliases

---
+++ src/07. aliases.ts
@@ -0,0 +1,13 @@
+type MyBoolean = true | false
+
+type Pair = [string, string]
+type Pairs = Pair[]
+
+type DiffCar = {
+  wheels: number
+  brand: string
+  type: string
+  isNew?: boolean
+  name?: string
+  [key: string]: unknown
+}
