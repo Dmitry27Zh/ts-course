7836a36c6998f503fa89607ae154ff8c86520e5b
Dmitry <dmitry27zh@gmail.com>
2023-10-03 20:38:34 +0300

    Interfaces vs aliases

---
+++ dist/02. intermediate/03. interfaces vs aliases.js
@@ -0,0 +1,3 @@
+"use strict";
+// Aliases
+// primitives, union...
---
+++ src/02. intermediate/03. interfaces vs aliases.ts
@@ -0,0 +1,12 @@
+// Aliases
+// primitives, union...
+
+// type Window = {} ---- error
+
+// Interfaces
+// extend identificator
+// class with implements
+
+interface Window {
+  isExtend: true
+}
