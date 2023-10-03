ad800e0e823c9dcbbd3294ea668b360f964164d2
Dmitry <dmitry27zh@gmail.com>
2023-10-03 20:10:22 +0300

    Interfaces intermediate

--- dist/02. intermediate/01. interfaces.js
+++ dist/02. intermediate/01. interfaces.js
@@ -1,1 +1,13 @@
 "use strict";
+window.isAuth = true;
+// const user1: Employee = {} ---- error
+// class HardWorker implements Employee {
+// contractStart: Date
+// phone: number
+// email: string
+// password: string
+// isOnline?: boolean | undefined
+// name: string
+// lastName: string
+// yearOfBirth?: number | undefined
+// }
--- src/02. intermediate/01. interfaces.ts
+++ src/02. intermediate/01. interfaces.ts
@@ -1,1 +1,46 @@
-interface User {}
+interface User {
+  readonly email: string
+}
+
+interface User {
+  password: string
+  isOnline?: boolean
+}
+
+// const user: User = { ---- error
+//   email: 'some@smsm',
+// }
+
+// user.email = 'smsjsj' ---- error
+
+interface Window {
+  isAuth?: boolean
+}
+
+window.isAuth = true
+// window.isAdmin = false ---- error
+
+interface Person {
+  readonly name: string
+  readonly lastName: string
+  phone?: string
+  yearOfBirth?: number
+}
+
+interface Employee extends User, Person {
+  contractStart: Date
+  phone: string
+}
+
+// const user1: Employee = {} ---- error
+
+// class HardWorker implements Employee {
+// contractStart: Date
+// phone: number
+// email: string
+// password: string
+// isOnline?: boolean | undefined
+// name: string
+// lastName: string
+// yearOfBirth?: number | undefined
+// }
