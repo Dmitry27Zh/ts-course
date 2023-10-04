0ac08121fff650a398e1df83422b0dfd203f8f6a
Dmitry <dmitry27zh@gmail.com>
2023-10-04 12:56:00 +0300

    Exclamation mark

---
+++ dist/02. intermediate/10. exclamation-mark.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let word = null;
+const num = 10;
+if (num > 5) {
+    word = 'abc';
+}
+// console.log(word.toUpperCase()) ---- error
+console.log(word.toUpperCase());
+console.log(word.toUpperCase());
+function printName(name) {
+    // const fullName: string = name ---- error
+    const fullName = name;
+}
+function printName2(person) {
+    console.log(person.name);
+}
+const people = [
+    {
+        name: 'Josh',
+        age: 30,
+        sex: 'male',
+    },
+    {
+        name: 'Maria',
+        age: 30,
+        sex: 'female',
+    },
+];
+const femalePerson = people.find((person) => person.sex === 'female'); // Person | undefined
+const malePerson = people.find((person) => person.sex === 'male'); // Person
---
+++ src/02. intermediate/10. exclamation-mark.ts
@@ -0,0 +1,41 @@
+let word: string | null = null
+const num = 10
+
+if (num > 5) {
+  word = 'abc'
+}
+
+// console.log(word.toUpperCase()) ---- error
+console.log((word as string).toUpperCase())
+console.log(word!.toUpperCase())
+
+function printName(name?: string) {
+  // const fullName: string = name ---- error
+  const fullName: string = name!
+}
+
+export interface Person {
+  name: string
+  age: number
+  sex: 'male' | 'female'
+}
+
+function printName2(person?: Person) {
+  console.log(person!.name)
+}
+
+const people: Person[] = [
+  {
+    name: 'Josh',
+    age: 30,
+    sex: 'male',
+  },
+  {
+    name: 'Maria',
+    age: 30,
+    sex: 'female',
+  },
+]
+
+const femalePerson = people.find((person) => person.sex === 'female') // Person | undefined
+const malePerson = people.find((person) => person.sex === 'male')! // Person
