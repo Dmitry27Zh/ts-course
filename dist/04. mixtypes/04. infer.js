5d242f4945047c5a1e11cfdb8a0974080782367a
Dmitry <dmitry27zh@gmail.com>
2023-10-08 11:41:23 +0300

    Infer

---
+++ dist/04. mixtypes/04. infer.js
@@ -0,0 +1,14 @@
+"use strict";
+function fromPair(pair) {
+    const [key, value] = pair;
+    return {
+        [key]: value,
+    };
+}
+const myPair = ['myKey', 'myValue'];
+fromPair(myPair);
+class Computer {
+    constructor(brand) { }
+}
+let brand;
+let dateArg;
