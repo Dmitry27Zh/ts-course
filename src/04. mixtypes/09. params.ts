84bfb835070dbdd7e7e2ce04a057d77ec684216d
Dmitry <dmitry27zh@gmail.com>
2023-10-08 13:05:00 +0300

    Params

---
+++ src/04. mixtypes/09. params.ts
@@ -0,0 +1,16 @@
+export {}
+
+function f1(arg: { a: number; b: string }) {
+  return arg.a + arg.b
+}
+
+type T0 = ReturnType<typeof f1>
+type T1 = Parameters<typeof f1>
+type T2 = ConstructorParameters<ErrorConstructor>
+
+class Car {
+  constructor(brand: string, model: string) {}
+}
+
+// type T3 = ConstructorParameters<Car> ---- error: Class identificator is instance type
+type T3 = ConstructorParameters<typeof Car>
